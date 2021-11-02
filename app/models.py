cafc02f83c08cc9dd5a34d8e776d57e28adb0fa9
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-11-02 11:28:31 -0600

    feat: init api setup
    - Basic log models for validation
    - Route to test log creation in DB

---
+++ app/models.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+from typing import Optional
+
+from bson import ObjectId
+from pydantic import BaseModel, Field
+
+
+# MongoDB uses BSON but Python uses JSON so we must convert ObjectIds to strings
+class PyObjectId(ObjectId):
+    # Required methods for proper validation and updating
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError(f"{v} is not a valid ObjectId")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="str")
+
+
+class LogModel(BaseModel):
+    # Alias id to _id so we can refer to it by id and have Mongo handle it correctly
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")  # Default to PyObjectId
+    severity: str = Field(...)
+    message: str = Field(...)
+    timestamp: datetime = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "severity": "INFO",
+                "message": "This is a test message",
+                "timestamp": datetime.now(),
+            }
+        }
+
+
+class UpdateLogModel(BaseModel):
+    severity: Optional[str]
+    message: Optional[str]
+    timestamp: Optional[datetime]
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "severity": "INFO",
+                "message": "This is a test message",
+                "timestamp": datetime.now(),
+            }
+        }
